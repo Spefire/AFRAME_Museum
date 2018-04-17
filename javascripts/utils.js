738c86e3f50d90ea5b05460304c519a2f780b738
Nicholas BRUN <nicholasb@hotmail.fr>
2018-04-17 10:51:43 +0200

    Refractor utils header

--- javascripts/utils.js
+++ javascripts/utils.js
@@ -1,18 +1,8 @@
-// ---------------------------- //
-//   Application : Museum REV   //
-//   Author : Nicholas Brun     //
-//   File : utils.js    		//
-// ---------------------------- //
-
-function setRotation(obj, angleX, angleY, angleZ){
-	obj.rotation.x = angleX * Math.PI / 180;
-	obj.rotation.y = angleY * Math.PI / 180;
-	obj.rotation.z = angleZ * Math.PI / 180;
-}
-
-function getRadian(angle) {
-	return (angle * Math.PI / 180);
-}
+// -------------------------------- //
+//   Application : Museum AFRAME    //
+//   Author : Nicholas BRUN   		//
+//   File : utils.js				//
+// -------------------------------- //
 
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
